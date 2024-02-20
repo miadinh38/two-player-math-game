70ba0da8312f5aa48536037bd55d5fc5fa2e8512
Mia Dinh <miadinh38@gmail.com>
2024-02-19 23:14:18 -0500

    Implement is_answer_correct method

--- Question.rb
+++ Question.rb
@@ -1,13 +1,17 @@
 class Question
 
   def initialize
-    @num1 = rand(1..50)
-    @num2 = rand(1..50)
-    @answer = @num1 + @num2
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @correct_answer = @num1 + @num2
   end
 
   def ask_question
     "What does #{num1} plus #{num2} euqal?"
+  end
+
+  def is_answer_correct(guess)
+    guess == @correct_answer
   end
 
 end
